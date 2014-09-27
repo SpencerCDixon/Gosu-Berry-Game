cca5a3b9dff932444b08ec3f0ed40a1a650e3ee2
Spencer Dixon <spencercdixon@gmail.com>
2014-09-27 12:08:58 -0400

    Berries can now be drawn on the board with their quantity next to them

--- lib/farmer.rb
+++ lib/farmer.rb
@@ -6,10 +6,12 @@ class Farmer
     @y = y
 
     @farmer = Gosu::Image.new(window, 'img/farmer.png')
+    @text_bubble = Gosu::Image.new(window, 'img/text_bubble.png')
   end
 
   def draw
     @farmer.draw(@x, @y, 0)
+    @text_bubble.draw(@x - 100, @y - 250, 0)
   end
 
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require 'pry'
 
 require_relative 'lib/berry'
 require_relative 'lib/farmer'
@@ -7,13 +8,21 @@ require_relative 'lib/calendar'
 
 
 class Main < Gosu::Window
-attr_reader :month
+
+  attr_reader :month, :basket
+
   def initialize
     super(800, 600, false)
     self.caption = "Berry Game"
 
     @farmer = Farmer.new(self, 550, 300)
-    @calendar = Calendar.new(self, 30, 30)
+    @calendar = Calendar.new(self, 30, 50)
+
+    @basket = { yellow: 1, white: 1, black: 1, pink: 1,
+                orange: 1, blue: 1, green: 1, gray: 1,
+                red: 1, teal: 1, brown: 1, purple: 1 }
+
+    @berry_images = Berries.new(self, 50, 300)
 
   end
 
@@ -25,6 +34,20 @@ attr_reader :month
   def draw
     @farmer.draw
     @calendar.draw
+    @berry_images.draw
+  end
+
+
+  def combine_berries(berry1, berry2)
+
+  end
+
+  def generate_correct_berry(array)
+    array.sort!
+    case
+    when array == ["x", "y", "y", "y"] || ["y", "y", "y", "z"]
+      return "yellow"
+    end
   end
 
 end
